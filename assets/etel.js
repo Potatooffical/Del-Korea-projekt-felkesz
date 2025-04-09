4d61c1194751eb472cac6520b1876b6e2824b5d4
Potatooffical <danorb12@gmail.com>
2025-04-09 18:32:06 +0200

    ételes JS tömb és annak a étel js befejezése és annak a html oldal finommitása bootsrappal és cssel

--- assets/etel.js
+++ assets/etel.js
@@ -1,15 +1,20 @@
-fetch('')
-.then(x=> x.json())
-.then(y=> megjelenit(y))
-
-let sz="";
-function megjelenit(y){
-     sz+="<div class='row'>"
-   for (let i = 0; i < 9; i++) {
-     sz+=`<div class="col-lg-4" ></div>`
-     
-   }
-   sz+="</div>" 
-document.getElementById("etel").innerHTML=sz
-   }
-   
+//function meghívása és adatok js tömböl való kiiratása amely külön js fájlban van
+function etelkiir() {
+        let sz = `<div class="row">`;
+      
+        for (const elem of eteltomb) {  
+          sz += `
+            <div id="grid" class="col-sm-4">
+              <img class="img-fluid rounded" src="/kepek/${elem.kep}" alt="Etel ${elem.neve}" style="width:100%">
+              <p>${elem.neve}</p>
+              <h3>${elem.leiras}</h3>
+              <h4>${elem.tipusa}</h4>
+            </div>
+          `;
+        }
+        sz += `</div>`;
+        document.getElementById("etel").innerHTML = sz;
+      }
+// function meghívása
+etelkiir();
+      
