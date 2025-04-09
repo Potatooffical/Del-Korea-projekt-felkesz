10a52f75afd5786e5757ff09c904faeb3d203dd8
Niffty Its_me_ <danorb12@gmail.com>
2025-04-09 15:46:28 +0200

    Amivel haladtam js tomb tovább folytatása

--- assets/Eteltomb.js
+++ assets/Eteltomb.js
@@ -4,13 +4,62 @@ const eteltomb = [
     tipusa: "tésztaalapú étel",
     leiras:
       "Ez a kínai-koreai étel sok-sok gyermek (és felnőtt) szívét elnyerte már a koreai kultúrában. A tésztaalapú ételt feketebab szósszal készítik el, és hogy még ízletesebb legyen, sokszor marha vagy sertéshús és hagyma is kerül az ételbe, a tetejére pedig főtt tojás, uborkaszeletek és pirított szezámmag. Kitűnő választás, ha szeretnénk elkerülni a csípős ételeket, és mégis koreai ízekre vágyunk.",
-    kep: "/kepek/01.png",
+    kep: "/kepek/2.png",
   },
   {
     neve: "Kimchi Jjigae (김치찌개)",
     tipusa: "leves",
     leiras:
       "Szintén egy igazi klasszikusról beszélhetünk, amiben minden megtalálható, ami a tökéletes koreai ételhez szükséges: kimchi, sertéshús, tofu és sok-sok zöldség. Koreában gyakran rizzsel és egyéb körettel fogyasztják. A téli hónapokra tökéletes lehet ez a forró, csípős egytálétel, ami garantáltan megizzaszt majd.",
-    kep: "/kepek/02.png",
+    kep: "/kepek/3.png",
   },
+  {
+    neve: "Tangsuyuk (탕수육)",
+    tipusa: "",
+    leiras: "Még egy kínai-koreai eredetű étel, amit édes-savanyú ízeiért szeretnek. Bundában kisütött sertéshús-falatkákat tálalnak ízletes édes szószban, amibe sokszor hagymát, répát és néha akár ananászt vagy egyéb gyümölcsöt is kevernek. A jjajangmyeon mellé, vagy önmagában is fogyaszthatjuk.",
+    kep: "/kepek/4.png",
+  },
+  {
+    neve: "Tteokbokki (떡볶이)",
+    tipusa: "tésztaalapú étel",
+    leiras: "A tteokbokival gyakran találkozhatunk filmekben és sorozatokban, hiszen az utcai árusok legnépszerűbb ételéről és a koreaiak egyik kedvenc street foodjáról beszélünk. A cső alakú rizstészta-rudakat csilikrémből készült szószban tálalják és sokszor hagymát, halpogácsát és főtt tojást is adnak az ízletes fogáshoz. A tteokbokki olcsó, finom, könnyen elkészíthető étel, de fel kell készülni arra, hogy a csilikrém bizony nagyon csípős!",
+    kep: "/kepek/5.png",
+  },
+  {
+    neve:"Gamjatang (감자탕)",
+    tipusa:"tesztaleves",
+    leiras:"A sertéshús csontjából készült levesben krumplit, káposztát és zöldségeket is találunk a hús mellett. Ezt a nagyon egészséges ételt sokszor rizzsel fogyasztják a téli hónapokban, a magyar emberek húsleveshez szokott gyomrához pedig tökéletesen passzol majd ez a fogás!",
+    kep:"/kepek/6.png",
+  },
+  {
+    neve:"Hanwoo és Samgyeopsal (한우/삼겹살)",
+    tipusa:"",
+    leiras:"A Hanwoo az egyik legmagasabb minőségű marhahús Koreában, amit olyan finomnak tartanak, hogy sokszor magában, rizs nélkül esznek. Persze ennek a húsnak borsos ára van, így ha nincs kitömve a pénztárcánk, akkor is érdemes kipróbálnunk a koreai bbq-t, sütögetést. Az egyik legnépszerűbb fogás a samgyeopsal, azaz császárhús, amit az asztalon lévő grillen megsütve, szószokba mártva és gyakran salátalevélbe tekerve fogyaszthatunk.",
+    kep:"/kepek/7.png",
+  },
+  {
+    neve:"A koreai sült csirke",
+    tipusa:"Húsétel",
+    leiras:"Azt gondolnánk, hogy a sült csirkét mindenhol ugyanúgy fogyasztják a világon, de nem ez a helyzet a koreai csirkével, ahol többfajta szószban és módon kisütött csirkét is megkóstolhatunk. Találhatunk itt csípős és kevésbé csípős szószban megforgatott csirkét, újhagymás sült csirkét és a népszerű mézes-vajas ízű csirkét is. Ahogy a koreaiaknak, nekünk is nagyon jól fog esni egy korsó hideg sör a finom húsok mellé!",
+    kep:"/kepek/8.png",
+  },
+  {
+    neve:"Naengmyeon (냉면)",
+    tipusa:"leves",
+    leiras:"A naengmyeon hideg tésztaétel, a hajdinatésztát hideg, frissítő húsleveslében tálalják. Természetesen ez a nyári hónapokban tökéletes étel, és hogy még frissítőbb legyen, megbolondíthatjuk japán retekkel, körtével, főttt tojással és hússal is, a tetejére pedig ecetet vagy mustárt is önthetünk.",
+    kep:"/kepek/9.png",
+  },
+  {
+    neve:"",
+    tipusa:"",
+    leiras:"",
+    kep:"",
+  },
+  {
+    neve:"",
+    tipusa:"",
+    leiras:"",
+    kep:"",
+  },
+
 ];
